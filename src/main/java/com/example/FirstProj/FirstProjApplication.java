9654f5c380bed5b5d641d5edf644893a7ee37060
MahabirGupta <mahabirg81@gmail.com>
2024-08-07 15:12:48 +0800

    Create SpringBoot Project

--- src/main/java/com/example/FirstProj/FirstProjApplication.java
+++ src/main/java/com/example/FirstProj/FirstProjApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FirstProjApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FirstProjApplication.class, args);
+		System.out.println("Welcome to boot...");
 	}
 
 }
